f0216dab8a65ccad6551af47ef0419995a4cbefa
Javier Mayor Oyarzun <jmayorodam1@gmail.com>
2020-11-13 23:05:38 +0100

    Commit final

--- IUTexto.java
+++ IUTexto.java
@@ -60,6 +60,75 @@ public class IUTexto
             
 
         }
+        
+         if(numero2 != octal){
+
+            while (numero2 != 0){
+                int resto = numero2 % 10;
+                if(resto == 8 || resto == 9){
+                    System.out.println("No es octal");
+                }
+                numero2 /= 10;
+            }
+            
+
+        }
+        
+        if( numero1 != numero2){
+          int cifras1 = 0;
+        while(numero1 > 0){
+            numero1 = numero1/10;
+            cifras1++;
+
+        }
+        
+            int cifras2 = 0;
+        while(numero2 > 0){
+            numero2 = numero1/10;
+            cifras2++;
+
+        }
+        
+        if(cifras1 != cifras2){
+            System.out.println("Los numeros no tienen las mismas cifras");
+        
+        }
+        else if(cifras1 == cifras2){
+                   
+        int octal1 = 0;
+        int octal2 = 0;
+        int p = 0;
+
+        while(numero1 != 0){
+            int resto =  numero1 % 8;
+            octal1 += (int) Math.pow(10,p) * resto;
+            numero1 /= 8;
+            p++;
+
+        }
+        
+        p = 0;
+         while(numero2 != 0){
+            
+             int resto =  numero2 % 8;
+            octal2 += (int) Math.pow(10,p) * resto;
+            numero2 /= 8;
+            p++;
+
+        }
+        int aux = octal1 + octal2;
+        
+        
+        
+        }
+      }
+      
+       
+        // if( "s" == teclado.nextstring()  || "S" == teclado.nextstring()  ){
+        
+            
+        // }
+      
 
         
     }
